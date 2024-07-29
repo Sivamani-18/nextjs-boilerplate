d11102c52396cd9c4f9a22508304a7677a575956
Sivamani-18 <siva@mindwaveventures.com>
2024-07-29 13:44:23 +0530

    update for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  // Set the basePath to your repository name if it's not the root domain
-  basePath: '/nextjs-boilerplate', // Replace 'your-repo-name' with the actual name of your GitHub repository
-
-  // Set the assetPrefix to serve assets correctly
-  assetPrefix: '/nextjs-boilerplate/', // Ensure this matches your basePath
-
-  // Enable trailing slash to ensure paths work correctly
+  basePath: isProd ? '/nextjs-boilerplate' : '', // Use an empty string for development
+  assetPrefix: isProd
+    ? 'https://sivamani-18.github.io/nextjs-boilerplate/'
+    : undefined,
   trailingSlash: true,
-
-  // Optional: other Next.js configurations can go here
 };
 
 export default nextConfig;
