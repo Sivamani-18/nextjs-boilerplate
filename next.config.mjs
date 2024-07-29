655a867fcb73eff84a12e9d984ce924d9fae695d
Sivamani-18 <siva@mindwaveventures.com>
2024-07-29 13:18:03 +0530

    update  next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Set the basePath to your repository name if it's not the root domain
+  basePath: '/nextjs-boilerplate', // Replace 'your-repo-name' with the actual name of your GitHub repository
+
+  // Set the assetPrefix to serve assets correctly
+  assetPrefix: '/nextjs-boilerplate/', // Ensure this matches your basePath
+
+  // Enable trailing slash to ensure paths work correctly
+  trailingSlash: true,
+
+  // Optional: other Next.js configurations can go here
+};
 
 export default nextConfig;
